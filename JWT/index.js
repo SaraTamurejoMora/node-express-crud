da90c9e5acceef8a715fdce546a0689817b2c30e
Sara Tamurejo <sara.tamurejo.7e8@itb.cat>
2025-10-07 19:13:55 +0200

    no va

--- JWT/index.js
+++ JWT/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import { PORT, SECRET_JWT_KEY} from './config.js';
 
 
+import { UserRepository } from './user-repository.js';
+
+
 const app = express();
 
 app.use(express.json());
@@ -14,14 +17,36 @@ app.set('views', './views');
 //Incio de los endpoints
 
 app.get('/',(req,res) => {
-    res.render('login');
+    res.render('register');
 })
 
-app.post('/register', (req,res) => {
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ error: "Faltan datos" });
+    }
+
+    console.log("Nuevo usuario:", username, password);
+
+
+    res.status(201).json({ message: "Usuario registrado con éxito" });
+});
+
+
+
+app.post('/register', async (req,res) => {
     const {username,password} = req.body;
     console.log(username);
+try{
+    const id = await UserRepository.create({username,password})
+    res.send({id})
+}catch(error){
+    res.status(400).send(error.message);
+}
 
-
+    
 });
 
 app.listen(PORT,() => console.log(`Abierto en el puerto ${PORT}`))
